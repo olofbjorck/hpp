208eb2eb51e254a7fe0f026edd874217ba8571d7
Olof Björck <olofbjorck@student-210-66.eduroam.uu.se>
2019-02-01 10:30:25 +0100

    Added comments

--- assignment2/part1/triang.c
+++ assignment2/part1/triang.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Declare function
 int combination(int n, int k);
 
 int main(int argc, char** argv) {
 
+  // Get input from command line
 	int n = 1;
 	if(argc > 1) {
 		n = atoi(argv[1]);
 	}
 
+  // Print the combination
 	for(int i=0; i<n; i++) {
 		for(int j=0; j<=i; j++) {
 			printf("%d ", combination(i,j));
@@ -20,6 +23,7 @@ int main(int argc, char** argv) {
 	return 0;
 }
 
+// Compute the combination
 int combination(int n, int k) {
 	int sum = 1;
 	for(int i=1; i<=k; i++) {
--- assignment2/part2/readfile.c
+++ assignment2/part2/readfile.c
@@ -2,25 +2,29 @@
 
 int main() {
 
+  // Open file
 	FILE* f = fopen("little_bin_file", "r");
 
+  // Declare variables in which to store file input
 	int intVar;
 	double doubleVar;
 	char charVar;
 	float floatVar;
 
+  // Read file
 	fread(&intVar, sizeof(int), 1, f);
 	fread(&doubleVar, sizeof(double), 1, f);
 	fread(&charVar, sizeof(char), 1, f);
 	fread(&floatVar, sizeof(float), 1, f);
 
+  // Close file
 	fclose(f);
 
+  // Print what was read from the file
 	printf("%d\n", intVar);
 	printf("%lf\n", doubleVar);
 	printf("%c\n", charVar);
 	printf("%f\n", floatVar);
-
 
 	return 0;
 }
