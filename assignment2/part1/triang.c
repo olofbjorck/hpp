c3283c504b240c20f9c8fc3c65630a39c720c997
Gunnlaugur <gunnlaugurgeirs@gmail.com>
2019-02-02 13:40:50 +0100

    fixed bugs and commented code

--- assignment2/part1/triang.c
+++ assignment2/part1/triang.c
@@ -6,13 +6,13 @@ int combination(int n, int k);
 
 int main(int argc, char** argv) {
 
-  // Get input from command line
+	// Get input from command line
 	int n = 1;
 	if(argc > 1) {
 		n = atoi(argv[1]);
 	}
 
-  // Print the combination
+	// Print the combination
 	for(int i=0; i<n; i++) {
 		for(int j=0; j<=i; j++) {
 			printf("%d ", combination(i,j));
@@ -27,7 +27,7 @@ int main(int argc, char** argv) {
 int combination(int n, int k) {
 	int sum = 1;
 	for(int i=1; i<=k; i++) {
-		sum	= sum*(n+1-i)/i;
+		sum	= sum*(n+1-i)/i;	// avoid mult. assignment as int division floors
 	}
 	return sum;
 }
