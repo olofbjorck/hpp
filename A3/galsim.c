17028baf342857b8e60a52e4679f4e9a93472179
Olof Björck <olofbjorck@Olofs-MacBook-Pro.local>
2019-02-04 18:08:10 +0100

    Created read and write funcs. Working for N=2 but not N=10 (segfault 11).

--- A3/galsim.c
+++ A3/galsim.c
@@ -18,13 +18,16 @@ typedef struct particle {
 
 
 // Read input data
-void readData(particle_t*);
+void readData(particle_t* particles, const char* filename, int N);
 // Simulates the galaxies
 void simulate(particle_t*);
 // Saves final positions and velocities to result.gal
-void writeOutput(particle_t*);
+void writeOutput(particle_t* particles, int N);
 // Calculates plummer spheres force
 void calculateForces(particle_t*);
+
+// Print for debugging. Can choose to print first n particles.
+void printParticles(particle_t*, int);
 
 // Main function
 int main(int argc, char const *argv[]) {
@@ -37,7 +40,7 @@ int main(int argc, char const *argv[]) {
 
   // Read input from command line
   const int N = atoi(argv[1]); // Nr of stars to simulate
-  char* filename[strlen(argv[2])]; strcpy(*filename, argv[2]);
+  const char* filename = argv[2];
   const int nsteps = atoi(argv[3]); // Nr of filesteps
   const double delta_t = atof(argv[4]); // Timestep
   const int graphics = atoi(argv[5]); // Graphics on/off as 1/0
@@ -49,10 +52,72 @@ int main(int argc, char const *argv[]) {
 
   // Create array with all particles
   particle_t particles[N]; // Use malloc? N can be large?
-  readData(particles);
+  readData(particles, filename, N);
+  printParticles(particles, N);
   // calculateForces(particles);
-  simulate(particles); // calculateForces is part of the simulatio
-  writeOutput(particles);
+  simulate(particles); // calculateForces is part of the simulation
+  writeOutput(particles, N);
 
   return 0;
+}
+
+// Read data from file.
+void readData(particle_t* particles, const char* filename, int N) {
+
+  // Open file
+  FILE* fp = fopen(filename, "r");
+
+// TODO: Check if at end of file? Currently reads a new particle with vals = 0
+
+  // Read file
+  int i;
+  for (i = 0; i < N; i++) {
+    fread(&particles[i].x, sizeof(double), 1, fp);
+    fread(&particles[i].y, sizeof(double), 1, fp);
+    fread(&particles[i].mass, sizeof(double), 1, fp);
+    fread(&particles[i].vx, sizeof(double), 1, fp);
+    fread(&particles[i].vy, sizeof(double), 1, fp);
+    fread(&particles[i].brightness, sizeof(double), 1, fp);
+  }
+
+  // Close file
+  fclose(fp);
+}
+
+void simulate(particle_t* particles) {
+
+}
+
+// Write current state of all particles to file
+void writeOutput(particle_t* particles, int N) {
+
+  // Create file to write
+  FILE* fp = fopen("result.gal", "w");
+
+  // Write to file
+  int i;
+  for (i = 0; i < N; i++) {
+    fwrite(&particles[i].x, sizeof(double), 1, fp);
+    fwrite(&particles[i].y, sizeof(double), 1, fp);
+    fwrite(&particles[i].mass, sizeof(double), 1, fp);
+    fwrite(&particles[i].vx, sizeof(double), 1, fp);
+    fwrite(&particles[i].vy, sizeof(double), 1, fp);
+    fwrite(&particles[i].brightness, sizeof(double), 1, fp);
+  }
+
+  // Close file
+  fclose(fp);
+}
+
+// Print particles (for debugging)
+void printParticles(particle_t* particles, int N) {
+
+  int i;
+  for (i = 0; i < N; i++) {
+    printf("Particle %d:\n", i);
+    printf("\t(x, y) = (%.2lf, %.2lf)\n", particles[i].x, particles[i].y);
+    printf("\t(vx, vy) = (%.2lf, %.2lf)\n", particles[i].vx, particles[i].vy);
+    printf("\tmass = %lf\n", particles[i].mass);
+    printf("\tbrightness = %lf\n", particles[i].brightness);
+  }
 }
