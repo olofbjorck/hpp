123beff73f7d0580d69a1e6d6192c9faeefec50e
Olof Björck <olofbjorck@Olofs-MacBook-Pro.local>
2019-02-05 16:34:30 +0100

    Wrote updateParticles function.

--- A3/galsim.c
+++ A3/galsim.c
@@ -14,8 +14,8 @@
 // kan börja med att implementera med en struct och få allting att fungera,
 // sedan jobba på optimeringen.
 typedef struct particle {
-	double x, vx; // x-position x and x-velocity vx
-	double y, vy; // y-position y and y-velocity vy
+	double x, v_x; // x-position x and x-velocity vx
+	double y, v_y; // y-position y and y-velocity vy
 	double mass; // particle mass
   double brightness; // particle brightness
   double force_x, force_y; // Force exerted on particle
@@ -26,7 +26,8 @@ typedef struct particle {
 int readData(particle_t* particles, const char* filename, int N);
 // Simulate the movement of the particles
 void simulate(particle_t* particles, int N,
-              double G, double eps0, double delta_t);
+              double G, double eps0,
+              int nsteps, double delta_t);
 // Calculates plummer spheres force
 void calculateForces(particle_t* particles, int N,
                       double G, double eps0);
@@ -51,14 +52,14 @@ int main(int argc, char const *argv[]) {
 
   // Read input from command line
   const int N = atoi(argv[1]); // Nr of stars to simulate
-  const char* filename = argv[2];
+  const char* filename = argv[2]; // Filename
   const int nsteps = atoi(argv[3]); // Nr of filesteps
   const double delta_t = atof(argv[4]); // Timestep
   const int graphics = atoi(argv[5]); // Graphics on/off as 1/0
 
   // Define some constants
-  const double G = 100/N;
-  const double eps0 = 0.001;
+  const double G = 100/N; // Gravitational constant
+  const double eps0 = 0.001; // Plummer sphere constant
   // delta_t should be 10^-5 = 0.00001 passed as input
 
   // Create array with all particles
@@ -68,10 +69,11 @@ int main(int argc, char const *argv[]) {
   if (!readData(particles, filename, N)) return 0;
   printParticles(particles, N);
 
-  // calculateForces(particles);
-  simulate(particles, N, G, eps0, delta_t); // calculateForces is part of the simulation
+  // Simulate movement
+  simulate(particles, N, G, eps0, nsteps, delta_t);
   printParticles(particles, N);
 
+  // Write new state of particles to file
   writeOutput(particles, N);
 
   return 0;
@@ -79,10 +81,14 @@ int main(int argc, char const *argv[]) {
 
 // Simulate the movement of the particles
 void simulate(particle_t* particles, int N,
-              double G, double eps0, double delta_t) {
+              double G, double eps0,
+              int nsteps, double delta_t) {
 
-  calculateForces(particles, N, G, eps0);
-  updateParticles(particles, N, delta_t);
+  int i;
+  for (i = 0; i < nsteps; i++) {
+    calculateForces(particles, N, G, eps0);
+    updateParticles(particles, N, delta_t);
+  }
 }
 
 void calculateForces(particle_t* particles, int N, double G, double eps0) {
@@ -93,32 +99,56 @@ void calculateForces(particle_t* particles, int N, double G, double eps0) {
   for (i = 0; i < N; i++) {
     // Initialize forces
     particles[i].force_x = 0.0, particles[i].force_y = 0.0;
-    // Calculate
-    for (j = 0; j < N; j++) {
-      if (j != i) { // This check is unnecessary. Fix when optimizing.
-        // maybe do two for-loops instead, skipping i = j? 
-        // Calculate r-vector
-        r_x = particles[i].x - particles[j].x;
-        r_y = particles[i].y - particles[j].y;
-        r = sqrt(r_x*r_x + r_y*r_y);
-        // Calculate denominator
-        denom = r + eps0;
-        denom = denom*denom*denom;
-        // Calculate force
-        particles[i].force_x += particles[j].mass*r_x/denom;
-        particles[i].force_y += particles[j].mass*r_y/denom;
-      }
+    // Loop up until i
+    for (j = 0; j < i; j++) {
+      // Calculate r-vector
+      r_x = particles[i].x - particles[j].x;
+      r_y = particles[i].y - particles[j].y;
+      r = sqrt(r_x*r_x + r_y*r_y);
+      // Calculate denominator
+      denom = r + eps0;
+      denom = denom*denom*denom;
+      // Calculate force
+      particles[i].force_x += particles[j].mass*r_x/denom;
+      particles[i].force_y += particles[j].mass*r_y/denom;
+    }
+    // Now loop after i
+    for (j = i + 1; j < N; j++) {
+      // Calculate r-vector
+      r_x = particles[i].x - particles[j].x;
+      r_y = particles[i].y - particles[j].y;
+      r = sqrt(r_x*r_x + r_y*r_y);
+      // Calculate denominator
+      denom = r + eps0;
+      denom = denom*denom*denom;
+      // Calculate force
+      particles[i].force_x += particles[j].mass*r_x/denom;
+      particles[i].force_y += particles[j].mass*r_y/denom;
     }
     particles[i].force_x *= -G*particles[i].mass;
     particles[i].force_y *= -G*particles[i].mass;
   }
 }
 
+// Update the particles
 void updateParticles(particle_t* particles, int N,
                       double delta_t) {
 
+  int i; // Loop variable
+  double a_x, a_y; // Acceleration
+  // Looper over all particles
+  for (i = 0; i < N; i++) {
+    // Calculate acceleration
+    a_x = particles[i].force_x/particles[i].mass;
+    a_y = particles[i].force_y/particles[i].mass;
+    // Update velocity
+    particles[i].v_x += delta_t*a_x;
+    particles[i].v_y += delta_t*a_y;
+    // Update position
+    particles[i].x += delta_t*particles[i].v_x;
+    particles[i].y += delta_t*particles[i].v_y;
+  }
 }
-
 
 // Read data from file.
 int readData(particle_t* particles, const char* filename, int N) {
@@ -135,8 +165,8 @@ int readData(particle_t* particles, const char* filename, int N) {
       fread(&particles[i].x, sizeof(double), 1, fp);
       fread(&particles[i].y, sizeof(double), 1, fp);
       fread(&particles[i].mass, sizeof(double), 1, fp);
-      fread(&particles[i].vx, sizeof(double), 1, fp);
-      fread(&particles[i].vy, sizeof(double), 1, fp);
+      fread(&particles[i].v_x, sizeof(double), 1, fp);
+      fread(&particles[i].v_y, sizeof(double), 1, fp);
       fread(&particles[i].brightness, sizeof(double), 1, fp);
     }
 
@@ -162,8 +192,8 @@ void writeOutput(particle_t* particles, int N) {
     fwrite(&particles[i].x, sizeof(double), 1, fp);
     fwrite(&particles[i].y, sizeof(double), 1, fp);
     fwrite(&particles[i].mass, sizeof(double), 1, fp);
-    fwrite(&particles[i].vx, sizeof(double), 1, fp);
-    fwrite(&particles[i].vy, sizeof(double), 1, fp);
+    fwrite(&particles[i].v_x, sizeof(double), 1, fp);
+    fwrite(&particles[i].v_y, sizeof(double), 1, fp);
     fwrite(&particles[i].brightness, sizeof(double), 1, fp);
   }
 
@@ -176,9 +206,9 @@ void printParticles(particle_t* particles, int N) {
 
   int i;
   for (i = 0; i < N; i++) {
-    printf("Particle %d:\n", i);
+    printf("Particle %d:\n", i+1);
     printf("\t(x, y) = (%.2lf, %.2lf)\n", particles[i].x, particles[i].y);
-    printf("\t(vx, vy) = (%.2lf, %.2lf)\n", particles[i].vx, particles[i].vy);
+    printf("\t(vx, vy) = (%.2lf, %.2lf)\n", particles[i].v_x, particles[i].v_y);
     printf("\t(force_x, force_y) = (%.2lf, %.2lf)\n",
             particles[i].force_x, particles[i].force_y);
     printf("\tmass = %lf\n", particles[i].mass);
