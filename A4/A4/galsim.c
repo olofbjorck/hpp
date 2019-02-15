e08b33f1c3066527ab5d9b5a174202f327edffa6
Gunnlaugur <gunnlaugurgeirs@gmail.com>
2019-02-15 13:46:53 +0100

    Fixed warnings related to inline function declaration

--- A4/A4/galsim.c
+++ A4/A4/galsim.c
@@ -48,70 +48,12 @@ void simulateWithGraphics(
 }
 
 // Calculates force exerted on every particle
-extern inline void updateParticles(particle_t* __restrict particles,
+// See galsim.h for definition
+extern void updateParticles(particle_t* __restrict particles,
 		const int N,
 		const double G,
 		const double eps0,
-		const double delta_t) {
-
-	unsigned int i, j; // Loop iterators
-	double r = 0.0, r_x = 0.0, r_y = 0.0; // r-vector
-	double denom = 0.0; // Denominator
-
-	// Loop for first particle and initialize acceleration to 0.0
-	particles[0].a_x = 0.0;
-	particles[0].a_y = 0.0;
-	for (j = 1; j < N; j++) {
-		particles[j].a_x = 0.0;
-		particles[j].a_y = 0.0;
-		// Calculate r-vector
-		r_x = particles[0].x - particles[j].x;
-		r_y = particles[0].y - particles[j].y;
-		r = sqrt(r_x*r_x + r_y*r_y);
-		// Calculate denominator
-		denom = r + eps0;
-		denom = 1/(denom*denom*denom); // 1 msec faster than using /denom below
-		// Calculate acceleration (a_x, a_y == 0 at start due to calloc)
-		particles[0].a_x += particles[j].mass*r_x*denom;
-		particles[0].a_y += particles[j].mass*r_y*denom;
-		// Calculate corresponding acceleration for other particle, using
-		// Newton's third law
-		particles[j].a_x -= particles[0].mass*r_x*denom;
-		particles[j].a_y -= particles[0].mass*r_y*denom;
-	}
-	// Update velocity
-	particles[0].v_x += -G*delta_t*particles[0].a_x;
-	particles[0].v_y += -G*delta_t*particles[0].a_y;
-	// Update position
-	particles[0].x += delta_t*particles[0].v_x;
-	particles[0].y += delta_t*particles[0].v_y;
-
-	// Loop remaining particles, assuming acceleration is properly initialized
-	for (i = 1; i < N; i++) {
-		for (j = i + 1; j < N; j++) {
-			// Calculate r-vector
-			r_x = particles[i].x - particles[j].x;
-			r_y = particles[i].y - particles[j].y;
-			r = sqrt(r_x*r_x + r_y*r_y);
-			// Calculate denominator
-			denom = r + eps0;
-			denom = 1/(denom*denom*denom); // 1 msec faster than using /denom below
-			// Calculate acceleration (a_x, a_y == 0 at start due to calloc)
-			particles[i].a_x += particles[j].mass*r_x*denom;
-			particles[i].a_y += particles[j].mass*r_y*denom;
-			// Calculate corresponding acceleration for other particle, using
-			// Newton's third law
-			particles[j].a_x -= particles[i].mass*r_x*denom;
-			particles[j].a_y -= particles[i].mass*r_y*denom;
-		}
-		// Update velocity
-		particles[i].v_x += -G*delta_t*particles[i].a_x;
-		particles[i].v_y += -G*delta_t*particles[i].a_y;
-		// Update position
-		particles[i].x += delta_t*particles[i].v_x;
-		particles[i].y += delta_t*particles[i].v_y;
-	}
-}
+		const double delta_t);
 
 // Show particles graphically
 inline void showGraphics(
